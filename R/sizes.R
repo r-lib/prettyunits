7fd729b2dc2f5e49b3c641fd883b56944d0b5f69
Gábor Csárdi <csardi.gabor@gmail.com>
2019-12-27 09:48:34 +0000

    New bytes styles
    Also closes #19.

--- R/sizes.R
+++ R/sizes.R
@@ -1,21 +1,88 @@
 
 #' Bytes in a human readable string
 #'
+#' Use `pretty_bytes()` to format bytes. `compute_bytes()` is the underlying
+#' engine that may be useful for custom formatting.
+#' 
 #' @param bytes Numeric vector, number of bytes.
+#' @param style Formatting style:
+#'   * `"default"` is the original `pretty_bytes` formatting, and it always
+#'     pads the output, so that all vector elements are of the same width,
+#'   * `"nopad"` is similar, but does not pad the output,
+#'   * `"6"` always uses 6 characters,
+#'   The `"6"` style is useful if it is important that the output always
+#'   has the same width (number of characters), e.g. in progress bars.
+#'   See some examples below.
 #' @return Character vector, the formatted sizes.
+#'   For `compute_bytes`, a data frame with columns `amount`, `unit`,
+#'   `negative`.
 #'
 #' @export
 #' @examples
-#' pretty_bytes(1337)
-#' pretty_bytes(133337)
-#' pretty_bytes(13333337)
-#' pretty_bytes(1333333337)
-#' pretty_bytes(133333333337)
-#' pretty_bytes(c(1000 * 1000, 1000 * 1000 - 1))
+#' bytes <- c(1337, 133337, 13333337, 1333333337, 133333333337)
+#' pretty_bytes(bytes)
+#' pretty_bytes(bytes, style = "nopad")
+#' pretty_bytes(bytes, style = "6")
 
-pretty_bytes <- function(bytes) {
+pretty_bytes <- function(bytes, style = c("default", "nopad", "6")) {
 
-  szs <- units_bytes(bytes)
+  style <- switch(
+    match.arg(style),
+    "default" = pretty_bytes_default,
+    "nopad" = pretty_bytes_nopad,
+    "6" = pretty_bytes_6
+  )
+
+  style(bytes)
+}
+
+#' @rdname pretty_bytes
+#' @param smallest_unit A character scalar, the smallest unit to use.
+#' @export
+
+compute_bytes <- function(bytes, smallest_unit = "B") {
+  units0 <- c("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+
+  stopifnot(
+    is.numeric(bytes),
+    is.character(smallest_unit),
+    length(smallest_unit) == 1,
+    !is.na(smallest_unit),
+    smallest_unit %in% units0
+  )
+
+  limits <- c(1000, 999950 * 1000 ^ (seq_len(length(units0) - 2) - 1))
+  low <- match(smallest_unit, units0)
+  units <- units0[low:length(units0)]
+  limits <- limits[low:length(limits)]
+
+  neg <- bytes < 0 & !is.na(bytes)
+  bytes <- abs(bytes)
+
+  mat <- matrix(rep(bytes, each = length(limits)), nrow = length(limits))
+  exponent <- length(limits) - colSums(mat < limits) + low - 1L
+  res <- bytes / 1000 ^ exponent
+  unit <- units[exponent - low + 2L]
+
+  ## Zero bytes
+  res[bytes == 0] <- 0
+  unit[bytes == 0] <- units[1]
+
+  ## NA and NaN bytes
+  res[is.na(bytes)] <- NA_real_
+  res[is.nan(bytes)] <- NaN
+  unit[is.na(bytes)] <- units0[low]     # Includes NaN as well
+
+  data.frame(
+    stringsAsFactors = FALSE,
+    amount = res,
+    unit = unit,
+    negative = neg
+  )
+}
+
+pretty_bytes_default <- function(bytes) {
+  szs <- compute_bytes(bytes)
   amt <- szs$amount
 
   ## String. For fractions we always show two fraction digits
@@ -25,31 +92,33 @@ pretty_bytes <- function(bytes) {
     sprintf("%.2f", ifelse(szs$negative, -1, 1) * amt)
   )
 
-  "%s %s" %s% list(res, szs$unit)
+  format(paste(res, szs$unit), justify = "right")
 }
 
-units_bytes <- function(bytes) {
-  stopifnot(is.numeric(bytes))
+pretty_bytes_nopad <- function(bytes) {
+  sub("^\\s+", "", pretty_bytes_default(bytes))
+}
 
-  units  <- c('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
-  limits <- c(1000, 999950 * 1000 ^ (seq_len(length(units) - 2) - 1))
+pretty_bytes_6 <- function(bytes) {
+  szs <- compute_bytes(bytes, smallest_unit = "kB")
+  amt <- szs$amount
 
-  neg <- bytes < 0 & !is.na(bytes)
-  bytes <- abs(bytes)
+  na   <- is.na(amt)
+  nan  <- is.nan(amt)
+  neg  <- !na & !nan & szs$negative
+  l10  <- !na & !nan & !neg & amt < 10
+  l100 <- !na & !nan & !neg & amt >= 10 & amt < 100
+  b100 <- !na & !nan & !neg & amt >= 100
 
-  mat <- matrix(rep(bytes, each = length(limits)), nrow = length(limits))
-  exponent <- length(limits) - colSums(mat < limits)
-  res <- round(bytes / 1000 ^ exponent, 2)
-  unit <- units[exponent + 1]
+  szs$unit[neg] <- "kB"  
 
-  ## Zero bytes
-  res[bytes == 0] <- 0
-  unit[bytes == 0] <- units[1]
+  famt <- character(length(amt))
+  famt[na] <- " NA"
+  famt[nan] <- "NaN"
+  famt[neg] <- "< 0"
+  famt[l10] <- sprintf("%.1f", amt[l10])
+  famt[l100] <- sprintf(" %.0f", amt[l100])
+  famt[b100] <- sprintf("%.0f", amt[b100])
 
-  ## NA and NaN bytes
-  res[is.na(bytes)] <- NA_real_
-  res[is.nan(bytes)] <- NaN
-  unit[is.na(bytes)] <- "B"            # Includes NaN as well
-
-  list(amount = res, unit = unit, negative = neg)
+  paste0(famt, " ", szs$unit)
 }
