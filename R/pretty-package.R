b6977329886c45bb90df5ffda925b5d5d48622b8
Gábor Csárdi <csardi.gabor@gmail.com>
2023-09-24 10:49:11 +0200

    Fix package manual page

--- R/pretty-package.R
+++ R/pretty-package.R
@@ -5,6 +5,8 @@
 #'  - Time intervals: '1337000' -> '15d 11h 23m 20s'.
 #'  - Vague time intervals: '2674000' -> 'about a month ago'.
 #'  - Bytes: '1337' -> '1.34 kB'.
+#'  - p-values: '0.00001' -> '<0.0001'.
+#'  - Colors: '#FF0000' -> 'red'.
 #'  - Quantities: '1239437' -> '1.24 M'.
 #' @docType package
 #' @name prettyunits
